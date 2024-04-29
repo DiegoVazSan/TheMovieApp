f15e330980781ff01451e8aef8409827f3d61465
Diego Vazquez Sanchez <diegov@Diegos-MacBook-Pro.local>
2024-04-29 16:38:16 -0600

    351. Structure JSON

---
+++ TheMovieApp/Models/Movies.swift
@@ -0,0 +1,25 @@
+//
+//  Movies.swift
+//  TheMovieApp
+//
+//  Created by Diego Vazquez Sanchez on 29/04/24.
+//
+
+import Foundation
+
+struct Movie: Codable {
+    
+    let page: Int
+    let results: [Result]
+    
+}
+
+// MARK: - Result
+struct Result: Codable, Identifiable {
+    
+    let id: Int
+    let poster_path: String
+    let original_title: String
+    let overview: String
+    
+}
