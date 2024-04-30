da7ce7d9d4a56427c828a5a0bd8e39550decc014
Diego Vazquez Sanchez <diegov@Diegos-MacBook-Pro.local>
2024-04-30 15:01:45 -0600

    Embed youtube videos in the app

--- TheMovieApp/Views/TrailerView.swift
+++ TheMovieApp/Views/TrailerView.swift
@@ -6,16 +6,22 @@
 //
 
 import SwiftUI
+import WebKit
 
 struct TrailerView: View {
     
     @StateObject var moviesVM: MoviesViewModel
     
     var body: some View {
-        VStack {
-            Text(moviesVM.title)
-        }.task {
-            await moviesVM.fetchVideo()
+        ZStack {
+            Color.black.ignoresSafeArea(edges: .all)
+            VideoView(videoID: moviesVM.key)
+                .frame(minHeight: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxHeight: UIScreen.main.bounds.height * 0.3)
+                .cornerRadius(12)
+                .padding(.all)
+                .task {
+                    await moviesVM.fetchVideo()
+                }
         }
     }
     
@@ -24,3 +30,19 @@ struct TrailerView: View {
 //#Preview {
 //    TrailerView()
 //}
+
+
+struct VideoView: UIViewRepresentable {
+    let videoID : String
+    
+    func makeUIView(context: Context) -> some WKWebView {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        guard let url = URL(string: "https://www.youtube.com/watch?v=\(videoID)") else { return }
+        uiView.scrollView.isScrollEnabled = false
+        uiView.load(URLRequest(url: url))
+    }
+    
+}
